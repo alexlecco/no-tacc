a703398190f54a2e42cfda3f422fd303f22c57d7
alexlecco <alexlecco24@gmail.com>
2019-12-08 13:00:49 -0300

    set fixed heigh to product and store cards

--- components/StoreCard.js
+++ components/StoreCard.js
@@ -71,8 +71,8 @@ const styles = StyleSheet.create({
     flex: 0.2,
     flexDirection: 'row',
     paddingTop: 10,
-    paddingHorizontal: 10
-    
+    paddingHorizontal: 10,
+    height: 120
   },
   photo: {
     width: 50,
