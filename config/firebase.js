b6ac73de284dbd4e7261c11a71442c8e51d81c51
IsaiasPC <isamed92@gmail.com>
2020-01-16 13:40:01 -0300

    test login w firebase 2

--- config/firebase.js
+++ config/firebase.js
@@ -2,13 +2,15 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAiQFTkUeOInS6qM5UQysZ-8wNaPy0FlhY",
+    // apiKey: "AIzaSyAiQFTkUeOInS6qM5UQysZ-8wNaPy0FlhY",
+    apiKey: "AIzaSyAMc4kbavMZEUFAf5mEqZFpXAxYtaw4HJc",
     authDomain: "prceliaco-1cfac.firebaseapp.com",
     databaseURL: "https://prceliaco-1cfac.firebaseio.com",
     projectId: "prceliaco-1cfac",
     storageBucket: "prceliaco-1cfac.appspot.com",
     messagingSenderId: "34893812883",
-    appId: "1:34893812883:web:7a4d0f64439cc1a6a32966",
+    // appId: "1:34893812883:web:7a4d0f64439cc1a6a32966",
+    appId: "1:34893812883:android:8bf3eb1636eba2ada32966",
     measurementId: "G-HWY6YYGY1F"
 };
 
